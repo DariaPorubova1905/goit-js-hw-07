23457dc30b0092840313b694eaca42020e00f9b1
DariaPorubova1905 <102602876+DariaPorubova1905@users.noreply.github.com>
2023-08-09 20:04:54 +0300

    02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulGallery = document.querySelector(".gallery");
+const markup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) => `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+ulGallery.insertAdjacentHTML("beforeend", markup);
+
+let galleryLightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 200,
+});
